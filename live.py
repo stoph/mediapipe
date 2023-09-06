4177754a59bdd86ff1d27a7b94dadeb63e8bfb4c
Christoph Khouri <christoph.khouri@gmail.com>
2023-09-06 13:06:53 -0400

    refactor

--- live.py
+++ live.py
@@ -1,3 +1,5 @@
+# https://github.com/google/mediapipe/blob/master/docs/solutions/hands.md
+
 import cv2
 import mediapipe as mp
 
@@ -6,29 +8,35 @@ def num_to_range(num, inMin, inMax, outMin, outMax):
 
 # Initialize MediaPipe Hand components
 mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles # Adds colors to digits
 mp_hands = mp.solutions.hands
 
 # Initialize webcam
 cap = cv2.VideoCapture(0)
 
 # Initialize MediaPipe Hands with default parameters
-with mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+with mp_hands.Hands(
+  static_image_mode=False, 
+  #model_complexity=0,
+  max_num_hands=2, 
+  min_detection_confidence=0.5, 
+  min_tracking_confidence=0.5) as hands:
 
   while cap.isOpened():
-    success, frame = cap.read()
+    success, image = cap.read()
     if not success:
-      print("Failed to grab frame")
+      print("Failed to grab image")
       break
-    frame = cv2.flip(frame, 1)
-    #frame = cv2.resize(frame, (800, 600))
-
-    # Convert the BGR image to RGB
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
+    
     # Process the image and get hand landmarks
+    #image = cv2.resize(image, (800, 600))
     image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
     image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    
+    image_height, image_width, _ = image.shape
 
     # Draw hand landmarks
     if results.multi_hand_landmarks:
@@ -41,20 +49,25 @@ with mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_conf
         print(f'{mp_hands.HandLandmark(finger_point).name}:')
         x = hand_landmarks.landmark[mp_hands.HandLandmark(finger_point).value].x
         x = num_to_range(x, 0, 1, 0, 255)
-        
         print(f'x: {x}')
 
         # for i in range(5):
         #   print(f'{mp_hands.HandLandmark(i).name}:')
         #   print(f'{hand_landmarks.landmark[mp_hands.HandLandmark(i).value]}')
           
-        mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(
+          image, 
+          hand_landmarks, 
+          mp_hands.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
         #print(hand_landmarks)
 
-    # Display the frame
-    cv2.imshow('Hand', frame)
+    # Display the image (flipped horizontally for selfie view)
+    cv2.imshow('Hands', cv2.flip(image, 1))
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Exit on ESC
+    if cv2.waitKey(5) & 0xFF == 27:
       break
 
 cap.release()
